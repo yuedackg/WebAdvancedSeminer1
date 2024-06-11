448333878e2858315179aaa223903c50397f25e0
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-11 09:05:13 +0900

    その他のSQLを追加

--- 0605/app.py
+++ 0605/app.py
@@ -43,10 +43,33 @@ def init():
     init_sub()
     return render_template( "index.html", msg="initボタンが押されました。", list="")
 
+def append_sub():
+    if request.method == "POST":
+        inUid = request.form.get("userid")
+        inPassword = request.form.get( "password")
+        print( inUid, inPassword)
+        strSql = '''insert into tusers ( uid, password ) values ("''' + inUid +  '''"," ''' +inPassword +'''")'''
+        print( strSql)
+        con = sqlite3.connect(DBname)
+        con.execute( strSql)
+        con.commit()
+        con.close()
+
 @app.route( "/append/", methods=["POST"])
 def append():
-    
+    append_sub()
     return render_template( "index.html", msg="appendボタンが押されました")
 
+@app.route( "/delete/", methods=["POST"])
+def delete():
+    if request.method == "POST":
+        uid = request.form.get( "deleteUid")
+        strSQL = '''delete from tusers where uid =="'''+ uid + '''"'''
+        print( strSQL)
+        con = sqlite3.connect( DBname)
+        con.execute( strSQL)
+        con.commit()
+        con.close()
+        return render_template( "index.html", msg="削除ボタンが押されました", lines="")
 if __name__ == "__main__":
     app.run( port=8000, debug=True)
