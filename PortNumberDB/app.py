7d7633a4280a0d9e024506c335bb9897841063f5
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-13 18:14:24 +0900

    update

--- PortNumberDB/app.py
+++ PortNumberDB/app.py
@@ -29,8 +29,11 @@ def append():
 @app.route( "/delete/", methods=["post"])
 def delete():
     return render_template( "delete.html")
+
 @app.route( "/init/", methods=["post"])
 def init():
     return render_template( "init.html")
+
 if __name__ == "__main__" :
     app.run( port=8000, debug=True)
+
