c2cfd2fd973f5989d2ab0eaa8d0f355cd8a47485
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-14 18:10:04 +0900

    更新の反映

--- 0614/app.py
+++ 0614/app.py
@@ -1,11 +1,24 @@
 from flask import Flask,render_template,request
 import sqlite3
 
+DBname = "exampleDB.db"
+
 app = Flask( __name__)
 
 @app.route( "/")
 def index():
 	return render_template( "index.html")
+
+@app.route( "/init/")
+def	init():
+	strSQL = '''if not exists tusers ( uid text, passwd text)'''
+	strSQL2='''insert into tusers(uid, passwd) values( "24000", "ueda")"'''
+	conn=sqlite3.connect(DBname)
+	conn.execute(strSQL)
+	conn.execute( strSQL2)
+	conn.commit()
+	conn.close()
+	return render_template( "index.html", msg="データベースの初期化を行いました。")	
 
 if __name__ == "__main__":
 	app.run( port=8000, debug=True)
