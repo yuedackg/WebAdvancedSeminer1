b5944b68f5b0b1fbffa510e5c10606d13eb2fcce
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-16 23:58:44 +0900

    sakuseityuu

--- PortNumberDB/app.py
+++ PortNumberDB/app.py
@@ -8,31 +8,77 @@ DBname = "exampleDataBase.db"
 def index():
     return render_template( "index.html")
 
-@app.route("/view/" , methods=["post"])
-def view():
+def view_sub():
     if request.method=="POST":
         strSQL = '''select * from tusers'''
+        data = []
         try:
             conn = sqlite3.connect( DBname)
             cur = conn.cursor()
             cur.execute( strSQL)
-            # conn.commit()
-            # conn.close()
+            for line in cur.fetchall():
+                l = []
+                for item in line:
+                    l.append( item)
+                
+                data.append( l)
+            
+            conn.close()
+            return data
         except:
             print("OperationalError")
-    return render_template( "view.html",lines = cur.fetchall() )
+        return data
+
+@app.route("/view/" , methods=["post"])
+def view():
+    return render_template( "view.html", lines=view_sub())
 
 @app.route( "/append/", methods=["post"])
 def append():
     return render_template( "append.html")
 
+@app.route( "/doAppend/", methods=["post"])
+def doAppend():
+    if request.method == "POST":
+        inUid = request.form.get( "uid")
+        inPass = request.form.get( "pass")
+        print( inUid, inPass)
+    strSQL = '''select * from tusers where uid='''+ inUid + ''''''
+    msg=""
+    try:
+        con = sqlite3.connect( DBname)
+        cur = con.cursor()
+        cur.execute( strSQL)
+        line = cur.fetchone() 
+        if line== None:
+            strSQL = '''insert into tusers( uid, passwd) values ('''+ inUid + ''', "''' + inPass+ '''")''' 
+            print( strSQL)
+            cur.execute( strSQL)
+            con.commit()
+        print( line)
+        con.close()
+        msg="データを追加しました"
+    except sqlite3.OperationalError:
+        msg="データの追加に失敗しました。"
+    return render_template( "doAppend.html", msg=msg)
 @app.route( "/delete/", methods=["post"])
 def delete():
     return render_template( "delete.html")
 
 @app.route( "/init/", methods=["post"])
 def init():
-    return render_template( "init.html")
+    strSQL = '''create table if not exists  tusers ( uid int , passwd text) '''
+    strSQLinsert = '''insert into tusers ( uid, passwd ) values ( 1, "check")'''
+    try:
+        conn = sqlite3.connect( DBname)
+        conn.execute( strSQL)
+        conn.execute( strSQLinsert)
+        conn.commit()
+        conn.close()
+    except sqlite3.OperationalError :
+        print( "sql error")
+        return render_template( "init.html", msg="DBの初期化・初期データの登録が正しくできませんでした")
+    return render_template( "init.html", msg="テーブルの作成と初期化　OK")
 
 if __name__ == "__main__" :
     app.run( port=8000, debug=True)
