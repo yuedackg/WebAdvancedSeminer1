19506eb448dc264d743470c563b70116ba5870d3
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-19 06:05:43 +0900

    追加

--- WebService/app.py
+++ WebService/app.py
@@ -1,6 +1,32 @@
-from Flask import flask , render_template, request
+from flask import Flask, render_template, request
 import sqlite3
 
-app = Flask( __main__)
+app = Flask( __name__)
 
+@app.route( "/")
+def index():
+	return render_template( "index.html", msg="")
+
+@app.route( "/dbInit/")
+def dbinit():
+	return render_template( "dbinit.html")
+
+@app.route( "/dbView/")
+def dbview():
+	return render_template( "dbview.html")
+
+@app.route( "/dbAppend/")
+def dbappend():
+	return render_template( "dbappend.html")
+
+@app.route( "/dbDelete/")
+def dbdelete():
+	return render_template( "dbdelete.html")
+
+@app.route( "/dbUpdate/")
+def dbupdate():
+	return render_template( "dbupdate.html")
+
+if __name__ == "__main__":
+	app.run( port=8000, debug=True)
 app.run()
