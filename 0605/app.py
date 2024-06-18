2089c42a1d6618969ea334aa451f8b4369c2ba77
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-18 10:27:04 +0900

    授業の開始前の状態

--- 0605/app.py
+++ 0605/app.py
@@ -104,37 +104,6 @@ def login():
 
 @app.route( "/update/" , methods=["post"])
 def update():
-    if request.method == "POST":
-        inUserid = request.form.get( "userId")
-        inPassword = request.form.get( "userBeforePassword")
-        inFixPassword = request.form.get( "userAfterPassword")
-        print( inUserid, inPassword, inFixPassword)
-        strSQL = '''update tusers set password="''' + inPassword + '''" where uid="''' + inUserid + '''"'''
-        print(  strSQL)
-        try:
-            conn = sqlite3.connect(DBname)
-            cur = conn.cursor()
-            cur.execute( strSQL)
-            if cur.fetchone() != None :
-                # データがある
-                try:
-                    print("更新処理")
-                    
-                except sqlite3.OperationalError:
-                    # 更新処理のエラー処理
-                    pass
-        except sqlite3.OperationalError:
-            # strSQLのエラー処理
-            pass
-
-        try:
-            strSQL = '''update from tusers set uid = inFixPassword where uid = inUserid and Password = inPassword'''
-            conn =sqlite3.connect(DBname)
-            cur = conn.cursor()
-            cur.execute(strSQL)
-        except sqlite3.OperationalError:
-            # SQLのエラー処理
-            pass
 
 if __name__ == "__main__":
     app.run( port=8000, debug=True)
