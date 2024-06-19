c5ed8534f9828a0c74531a5fdedad8d30db78621
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-19 09:08:17 +0900

    課題用ひな形

--- WebService/app.py
+++ WebService/app.py
@@ -9,23 +9,9 @@ def index():
 
 @app.route( "/dbInit/")
 def dbinit():
+	
 	return render_template( "dbinit.html")
 
-@app.route( "/dbView/")
-def dbview():
-	return render_template( "dbview.html")
-
-@app.route( "/dbAppend/")
-def dbappend():
-	return render_template( "dbappend.html")
-
-@app.route( "/dbDelete/")
-def dbdelete():
-	return render_template( "dbdelete.html")
-
-@app.route( "/dbUpdate/")
-def dbupdate():
-	return render_template( "dbupdate.html")
 
 if __name__ == "__main__":
 	app.run( port=8000, debug=True)
