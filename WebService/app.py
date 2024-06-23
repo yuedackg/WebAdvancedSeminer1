32ec0249da16d9a0facfbeabdade3e8ca9c4b18d
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-06-24 00:30:23 +0900

    add

--- WebService/app.py
+++ WebService/app.py
@@ -5,6 +5,7 @@ app = Flask( __name__)
 
 DBname = 'mydbname.db'
 TableName = 'TGAKUSEI'
+REQUEST_TYPE="POST"
 
 @app.route( "/")
 def index():
@@ -39,6 +40,97 @@ def	dbInitializeInsertDummyData():
 	except sqlite3.DatabaseError:
 		message = "db 更新失敗"
 	return render_template( "dbinit.html", msg=message)
+
+@app.route( "/dbView/")
+def dbViewA():
+	print( "dbViewA()...")
+	message=""
+	lines=[]
+	strSQL = 'select * from {TableName} '
+
+	try:
+		conn = sqlite3.connect( DBname)
+		cur = conn.cursor()
+		s =  strSQL.format( TableName=TableName)
+		print( "SQL:", s )
+		cur.execute(s)
+		for line in cur.fetchall():
+			print(line)
+			item = []
+			for i in line :
+				item.append( i)
+			lines.append (item)			
+		conn.close()
+	except sqlite3.DatabaseError as err :
+		message="データベースからの取り出しに失敗しました。"
+
+	return render_template( "dbview.html", msg=message, lines=lines)
+	
+
+# @app.route( "/dbView/", methods=["post"])
+# def dbView():
+# 	message=""
+# 	lines=[]
+# 	strSQL = 'select * from {TableName} '
+# 	if request.method==REQUEST_TYPE:
+# 		try:
+# 			conn = sqlite3.connect( DBname)
+# 			cur = conn.cursor()
+# 			cur.execute( strSQL.format( TableName=TableName))
+# 			for line in cur.fetchall():
+# 				print(line)
+# 				lines.append( line)
+# 			conn.close()
+# 		except sqlite3.DatabaseError as err :
+# 			message="データベースからの取り出しに失敗しました。"
+# 	return render_template( "dbview.html", msg=message)
+	
+@app.route( "/dbAppend/")
+def dbAppendA():
+	return render_template( "dbappend.html")
+
+@app.route( "/dbAppend/", methods=["POST"])
+def dbAppend():
+	print( "in dbAppend()...")
+	message=""
+	inNo=0
+	strSimei=""
+	inYear=0
+	if request.method==REQUEST_TYPE:
+		try:
+			inNo = int( request.form.get( "no"))
+			inYear = int( request.form.get( "birthY"))
+		except ValueError:
+			message="数字が正しく入力されていない"
+		strSimei = request.form.get( "simei")
+		print( inNo, strSimei, inYear, message)
+		strSQL = "select * from {TableName} where id={myId}"
+		strSQL = strSQL.format( TableName=TableName, myId=inNo)
+		try:
+			conn = sqlite3.connect( DBname)
+			cur=conn.cursor()
+			cur.execute( strSQL)
+			if cur.fetchone() == None :
+				print( "dataが存在しないために登録")
+				try:
+					strSQL='insert into {myTable} ( id, simei, birthYear ) values ( {myId}, "{mySimei}", {myYear}) '
+					strSQL = strSQL.format(myTable=TableName,myId=inNo, mySimei=strSimei, myYear=inYear)
+					conn.execute( strSQL)
+					message="データの登録完了"
+				except sqlite3.DatabaseError:
+					message="データの追加中にエラーが発生しました"
+					print(message)
+			else:
+				message="データが存在するために登録しない"
+				print( message)
+			conn.commit()
+			conn.close()
+		except sqlite3.DatabaseError as dberr:
+			print( "データベース操作中にエラーが発生しました。")
+			print( strSQL)
+			print( dberr)
+	return render_template( "dbappend.html" , msg=message)
+
 
 if __name__ == "__main__":
 	app.run( port=8000, debug=True)
