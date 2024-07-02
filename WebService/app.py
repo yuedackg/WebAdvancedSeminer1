83f951bc904dbc924963e412bccfc0c91babf7d8
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2024-07-02 10:02:02 +0900

    データ読み出し部分の作成

--- WebService/app.py
+++ WebService/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import random
 
 app = Flask( __name__)
 DBname = "myDatabase.db"
@@ -23,14 +24,39 @@ def dbInitialize():
 	if request.method == REQUEST_TYPE:
 		con = sqlite3.connect( DBname)
 		con.execute( strSQL.format( TableName))
+		s='insert into {} ( gakusekiNo, simei, birthYear) values ( {}, "{}", {}) '
+		for k in range(5):
+			sql = s.format( TableName, random.randint( 23900, 23999), "dummy", random.randint( 2000, 2003) )
+			con.execute( sql)
 		con.commit()
 		con.close()
 		message = "データベースを作成しました"
+
 	return render_template( "index.html", msg=message)
 
 @app.route( "/dbView/")
 def dbView():
-	return render_template("dbview.html")
+	print( "dbView()...")
+	message=""
+	lines=[]
+	strSQL = 'select * from {TableName}'
+	
+
+	try:
+		con = sqlite3.connect( DBname)
+		cur = con.cursor()
+		s = strSQL.format( TableName=TableName)
+		cur.execute( s)
+		for line  in cur.fetchall():
+			item =[] 
+			for i in line:
+				item.append( i)
+			lines.append( item)
+		con.close()
+	except sqlite3.DatabaseError:
+		message="データベースからの取り出しに失敗しました。"
+
+	return render_template("dbview.html", msg=message, lines=lines)
 
 @app.route( "/dbAppend/")
 def dbAppend():
